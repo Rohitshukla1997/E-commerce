3be7626cf483b0fe51e9c05bc84c2c78cbbf3e48
Rohitshukla1997 <Rohitshukla1997>
2023-04-09 19:01:29 +0530

    first and completed commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+
+<div align="center"> <h1>WELCOME TO LOGOUT PAGE.</h1>
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ ('Dashboard') }}
+        </h2>
+    </x-slot>
+
+</x-app-layout>
